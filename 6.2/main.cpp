67370adbecd0f827c16cd1d611af658d54323b15
LarsSaalbrink <LarsSaalbrink@gmail.com>
2023-01-21 17:57:03 +0100

    Exercise 6.2

---
+++ 6.2/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip>
+#include <Safety.h>
+#include <UI.h>
+
+#define amountofDice 5
+#define maxRollAmount 10000000
+
+void programDone(){
+    std::cout << "Thank you for using the program!\n";
+}
+
+double rollDice(int rollAmount){
+    double diceRollTally = 0;
+    for(int i = 0; i < rollAmount; i++){  //Rolling "rollAmount" amount of times
+        for(int j = 0; j < amountofDice; j++){ //5 dice each time
+            diceRollTally += (rand() % 6) + 1;
+        }
+    }
+    return diceRollTally;
+}
+
+int main()
+{    
+    while(1){
+        Options currentChoice = choice2("\nRoll dice or exit? (R/E): ", 'R', 'E');
+        if(currentChoice == option1){
+            //Option 'R' selected
+            std::cout << "Choose amount of times to roll the 5 dice, max " << maxRollAmount << ": ";
+            unsigned int rollAmount = (unsigned int)(-1);
+
+            getWithinLimits(rollAmount, 1, maxRollAmount);
+
+            double diceRollTally = rollDice(rollAmount);
+            std::cout << "Rolling " << rollAmount << " times...\n";
+            std::cout << "Total across all rolls of all dice: " << diceRollTally << "\n";
+            std::cout << "Average across all rolls of all dice: " << (diceRollTally / (rollAmount*amountofDice)) << "\n";
+        }
+        else if(currentChoice == option2){
+            //Option 'E' selected
+            programDone();
+            break;
+        }
+        else{
+            //Invalid option selected
+            badInput();
+        }
+    }
+    return 0;
+}
---
+++ 6.2/out/lib/CMakeFiles/UI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UI.dir/UI.cpp.o"
+  "libUI.a"
+  "libUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
