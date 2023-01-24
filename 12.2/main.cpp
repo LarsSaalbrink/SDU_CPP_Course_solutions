475246d6de89ab9d97e12d9b653a2e8a9cf840bb
LarsSaalbrink <LarsSaalbrink@gmail.com>
2023-01-24 08:54:00 +0100

    Exercise 12.2, testing all types of tickets

--- 12.2/main.cpp
+++ 12.2/main.cpp
@@ -6,19 +6,39 @@ int main()
     //Set decimal precision to 2
     std::cout << std::fixed << std::cout.precision(2) << "\b";
 
+    //Create an uninitialized basic ticket
+    BasicTicket uninitializedTicket = BasicTicket();
+
     //Create a basic ticket
     BasicTicket basicTicket(1, 1, "Odense", "Prebens Classroom", 42.0);
+
+    //Create an uninitialized floor ticket
+    FloorTicket uninitializedFloorTicket = FloorTicket();
 
     //Create a floor ticket
     FloorTicket floorTicket(2, 2, "Odense", "Prebens Classroom", 42.0);
 
     //Print the tickets
-    std::cout << "Basic ticket:\n";
+    std::cout << "Uninitialized basic ticket:\n";
+    std::cout << "Seat: " << uninitializedTicket.getSeat();
+    std::cout << "\nTicket number: " << uninitializedTicket.getTicketNumber();
+    std::cout << "\nCity: " << uninitializedTicket.getCity();
+    std::cout << "\nOpera: " << uninitializedTicket.getOpera();
+    std::cout << "\nPrice: " << uninitializedTicket.getPrice();
+
+    std::cout << "\n\nBasic ticket:\n";
     std::cout << "Seat: " << basicTicket.getSeat();
     std::cout << "\nTicket number: " << basicTicket.getTicketNumber();
     std::cout << "\nCity: " << basicTicket.getCity();
     std::cout << "\nOpera: " << basicTicket.getOpera();
     std::cout << "\nPrice: " << basicTicket.getPrice();
+
+    std::cout << "\n\nUninitialized floor ticket:\n";
+    std::cout << "Seat: " << uninitializedFloorTicket.getSeat();
+    std::cout << "\nTicket number: " << uninitializedFloorTicket.getTicketNumber();
+    std::cout << "\nCity: " << uninitializedFloorTicket.getCity();
+    std::cout << "\nOpera: " << uninitializedFloorTicket.getOpera();
+    std::cout << "\nPrice: " << uninitializedFloorTicket.getPrice();
 
     std::cout << "\n\nFloor ticket:\n";
     std::cout << "Seat: " << floorTicket.getSeat();
